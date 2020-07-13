2698f44352d32994c9e0e392086b975cfe911e88
aledaas <aledaas@gmail.com>
2020-07-12 21:07:35 -0300

    Add endpoint leotweets

---
+++ src/github.com/aledaas/tiwtgo/routers/leoTweets.go
@@ -0,0 +1,37 @@
+package routers
+
+import (
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/aledaas/tiwtgo/bd"
+)
+
+/*LeoTweets lee los tweets*/
+func LeoTweets(w http.ResponseWriter, r *http.Request) {
+	ID := r.URL.Query().Get("id")
+	if len(ID) < 1 {
+		http.Error(w, "Debe enviar el parametro ID", http.StatusBadRequest)
+		return
+	}
+	if len(r.URL.Query().Get("pagina")) < 1 {
+		http.Error(w, "Debe enviar el parametro Página", http.StatusBadRequest)
+		return
+	}
+	pagina, err := strconv.Atoi(r.URL.Query().Get("pagina"))
+	if err != nil {
+		http.Error(w, "Debe enviar el parámetro pagina mayor a cero"+err.Error(), 400)
+		return
+	}
+	pag := int64(pagina)
+	respueta, correcto := bd.LeoTweets(ID, pag)
+	if correcto == false {
+		http.Error(w, "Error al leer los tweets", http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("context-type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(respueta)
+}
